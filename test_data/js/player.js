7f482085c3e3b8966f97629a3818f8bcc7521e57
时沧 <wangchengwang.wcw@alibaba-inc.com>
2026-03-03 11:40:29 +0800

    缩减行数避免自动化找不到对应行的元素

--- test_data/js/player.js
+++ test_data/js/player.js
@@ -2,21 +2,15 @@ class Player {
 	constructor(gameEngine) {
 		// 引用游戏主引擎实例，用于访问游戏状态和其他系统
 		this.gameEngine = gameEngine;
-
 		// 玩家初始世界坐标位置（像素单位）
 		this.x = 400; // 初始位置X
 		this.y = 300; // 初始位置Y
-
 		this.size = 12;
 		this.speed = 2;
-		this.direction = 'right';
-		this.nextDirection = null;
-		this.animationFrame = 0;
 		this.mouthOpen = true;
 		this.lastMoveTime = 0;
 		this.reset();
 	}
-
 	reset() {
 		if (this.gameEngine.maze) {
 			const centerRow = Math.floor(this.gameEngine.maze.rows / 2);
@@ -28,10 +22,6 @@ class Player {
 			this.x = 400;
 			this.y = 300;
 		}
-
-		this.direction = 'right';
-		this.nextDirection = null;
-		this.animationFrame = 0;
 		this.mouthOpen = true;
 	}
 }
